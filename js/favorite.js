29903629520dd5cc85dbac0f828e6fe386d504c6
AM <blackpunkrose12@gmail.com>
2024-05-16 17:04:44 +0900

    好きなことHPの作業ディレクトリを追加

---
+++ js/favorite.js
@@ -0,0 +1,28 @@
+const changeimg = document.getElementById("img1");
+    changeimg.addEventListener( 'mouseenter', () => {
+        changeimg.src = "./image/CR_off.jpg";
+    } );
+    changeimg.addEventListener( 'mouseleave', () => {
+        changeimg.src = "./image/CR_on.jpg";
+    } );
+const changeimg = document.getElementById("img2");
+    changeimg.addEventListener( 'mouseenter', () => {
+        changeimg.src = "./image/streamer_on.jpg";
+    } );
+    changeimg.addEventListener( 'mouseleave', () => {
+        changeimg.src = "./image/streamer_off.jpg";
+    } );
+const changeimg = document.getElementById("img3");
+    changeimg.addEventListener( 'mouseenter', () => {
+        changeimg.src = "./image/CR_off.jpg";
+    } );
+    changeimg.addEventListener( 'mouseleave', () => {
+        changeimg.src = "./image/vtuber.png";
+    } );
+const changeimg = document.getElementById("img4");
+    changeimg.addEventListener( 'mouseenter', () => {
+        changeimg.src = "./image/pro_on.jpg";
+    } );
+    changeimg.addEventListener( 'mouseleave', () => {
+        changeimg.src = "./image/pro_off.jpg";
+    } );
