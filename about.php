efc147f7f913a8a09d88f0a44bce4dccc1dc9172
AMbidextrous-GH <165616631+AMbidextrous-GH@users.noreply.github.com>
2024-07-23 14:40:45 +0900

    about部分の追加
    AJAX/JSONを使って、about.phpの概要欄(テキストエリア)にabout_text.phpの概要内容を表示できるように更新

---
+++ about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/about.css">
+    <title>About</title>
+</head>
+<body>
+    <div class="container_about"> 
+        <div class="about_text">
+            <textarea class="about-textarea" readonly></textarea>
+        </div>
+        <div class="about_img">
+            <img src="./img/about1.png" alt="About Image" class="about-image">
+        </div>
+    </div>
+    <script type="text/javascript" src="./js/about.js"></script>
+</body>
+</html>
+   
